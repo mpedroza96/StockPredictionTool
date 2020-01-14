9d568eadc4b6f760b23151c046b802166a0c8c8b
Mateo Pedroza <m.pedroza@uniandes.edu.co>
2020-01-14 17:07:57 -0500

    Plots and prediction working

---
+++ Interface.py
@@ -0,0 +1,88 @@
+from tkinter import *
+from tkinter.ttk import *
+import matplotlib
+import Tool as Tol
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+#Style
+
+#Window Configuration
+ventana=Tk()
+ventana.title("Stock Prediction Tool")
+ventana.geometry("800x300")
+ventana.configure(background="SkyBlue4")
+ventana.grid()
+
+#Labels
+label1 = Label(ventana, text="Name a stock (e.g. 'AAPL')")
+label1.configure(background="SkyBlue4", font = ('helvetica', 12))
+label1.grid(row=3, column=2)
+
+labele = Label(ventana, text=" ")
+labele.configure(background="SkyBlue4")
+labele.grid(row=2, column=0)
+labele = Label(ventana, text=" ")
+labele.configure(background="SkyBlue4")
+labele.grid(row=5, column=0)
+
+label2 = Label(ventana, text="Stock Prediction Tool")
+label2.configure(background="SkyBlue4", font = ('helvetica', 20, 'bold'))
+label2.grid(row=0, column=2)
+
+#Textbox inputs
+entry = Entry(ventana, text="Name a stock")
+entry.grid(row=4, column=2)
+
+#Tool class methods callouts
+def stockPlot():
+    stock = entry.get()
+    Tol.Tool.stockPlot(Tol.Tool.stockQuote(stock))
+
+def stockTable():
+    stock = entry.get()
+    Tol.Tool.stockPlot(Tol.Tool.stockQuote(stock))
+
+def trainModel():
+    stock = entry.get()
+    labelE = Label(ventana, text=("Model Error (RMSE): ", round(Tol.Tool.trainData(Tol.Tool.stockQuote(stock)), 2)))
+    labelE.configure(background="SkyBlue4", font=('helvetica', 10))
+    labelE.grid(row=8, column=2)
+
+def predictedPlot():
+    stock = entry.get()
+    Tol.Tool.predictionPlot()
+
+def predictedTable():
+    stock = entry.get()
+    Tol.Tool.predictionPlot()
+
+def prediction():
+    stock = entry.get()
+    labelE = Label(ventana, text=("Predicted Stock Price", round(Tol.Tool.futurePrediction(), 2)))
+    labelE.configure(background="SkyBlue4", font=('helvetica', 10))
+    labelE.grid(row=11, column=2)
+
+
+#Buttons
+button1 = Button(ventana, text="Stock closing price table")
+button1.grid(row=6, column=1)
+
+button2 = Button(ventana, text="Stock closing price plot", command=stockPlot)
+button2.grid(row=6, column=3)
+
+button3 = Button(ventana, text="Train ML LTSM Model (Wait 5 minutes)", command=trainModel)
+button3.grid(row=7, column=2)
+
+button4 = Button(ventana, text="Prediction closing price table (Historical Data)", command = predictedTable)
+button4.grid(row=9, column=1)
+
+button5 = Button(ventana, text="Prediction closing price plot (Historical Data)", command=predictedPlot)
+button5.grid(row=9, column=3)
+
+button6 = Button(ventana, text="Predicted Closing Price for Tomorrow", command=prediction)
+button6.grid(row=10, column=2)
+
+#Run interface
+ventana.mainloop()
---
+++ Tool.py
@@ -0,0 +1,143 @@
+#Stock prediction tool that uses Long Short Term Memory or LSTM to predict
+# closing price on a particular stock
+
+#Imports
+from datetime import datetime
+import math
+import pandas_datareader as web
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+plt.style.use('fivethirtyeight')
+
+class Tool:
+    #Variables
+    predictionModel = []
+    model = []
+    trainingDataLen = 0
+    data = []
+
+    def __init__(self):
+        self
+
+    #Stock quote history
+    def stockQuote(stockName):
+        return web.DataReader(stockName, data_source='yahoo', start= '2010-01-01', end= datetime.date(datetime.now()))
+
+    #Show plot of stock prices
+    def stockPlot(stock):
+        plt.figure(figsize=(15, 8))
+        plt.title('Price History')
+        plt.plot(stock['Close'])
+        plt.xlabel('Date', fontsize=20)
+        plt.ylabel('Price USD', fontsize=20)
+        plt.show()
+
+    #Show plot of stock prices
+    def stockTable(stock):
+        plt.title('Price History')
+        plt.show()
+
+    #Convert and train data with LSTM
+    def trainData(stock):
+        data = stock.filter(['Close'])
+        Tool.data = data
+        dataset = data.values
+        trainingDataLen = math.ceil(len(dataset)*0.8)
+        Tool.trainingDataLen = trainingDataLen
+
+        #Scale Data
+        scaler = MinMaxScaler(feature_range=(0,1))
+        scaledData = scaler.fit_transform(dataset)
+        trainData = scaledData[0:trainingDataLen]
+
+        #Split Data
+        x_train = []
+        y_train = []
+
+        for i in range(60, len(trainData)):
+            x_train.append(trainData[i-60:i, 0])
+            y_train.append(trainData[i, 0])
+
+        #Convert train data to numpy arrays
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+        #Reshape data
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+        #Build LSTM Model
+        model = Sequential()
+        model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+        model.add(LSTM(50, return_sequences=False))
+        model.add(Dense(25))
+        model.add(Dense(1))
+
+        model.compile(optimizer='adam', loss='mean_squared_error')
+
+        Tool.model = model
+
+        #Train model
+        model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+        #Create testing Data
+        testData = scaledData[trainingDataLen - 60: , :]
+        x_test = []
+        y_test = dataset[trainingDataLen: , :]
+
+        for i in range(60, len(testData)):
+            x_test.append(testData[i-60:i, 0])
+
+        #Convert data to numpy array
+        x_test = np.array(x_test)
+
+        #Reshape Data
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+        #Get models predicted values
+        predictions = model.predict(x_test)
+        predictions = scaler.inverse_transform(predictions)
+
+        #Calculate Error RMSE
+        rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+        print('RMSE for predictions: ', rmse)
+        Tool.predictionModel = predictions
+
+        return rmse
+
+    def predictionPlot():
+        if(Tool.data is None or Tool.predictionModel is None or Tool.trainingDataLen is None):
+            print('Train model before plot')
+        else:
+            train = Tool.data[:Tool.trainingDataLen]
+            valid = Tool.data[Tool.trainingDataLen:]
+            valid['Predictions'] = Tool.predictionModel
+            plt.figure(figsize=(15, 8))
+            plt.title('Prediction Model')
+            plt.xlabel('Date', fontsize=20)
+            plt.ylabel('Price USD', fontsize=20)
+            plt.plot(train['Close'])
+            plt.plot(valid[['Close', 'Predictions']])
+            plt.legend(['Data used for training model', 'Real Value', 'Predictions'], loc='lower right')
+            plt.show()
+
+    def futurePrediction():
+        lastDays = Tool.data[-60:].values
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        lastDaysScaled = scaler.fit_transform(lastDays)
+        x_test = []
+        x_test.append(lastDaysScaled)
+        x_test = np.array(x_test)
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+        pred = Tool.model.predict(x_test)
+        pred = scaler.inverse_transform(pred)
+        return pred
+
+
+
+#Tool.stockPlot(Tool.stockQuote('AAPL'))
+#Tool.futurePrediction(Tool.data, Tool.model)
+#Tool.trainData(Tool.stockQuote('AAPL'))
+#Tool.predictionPlot()
